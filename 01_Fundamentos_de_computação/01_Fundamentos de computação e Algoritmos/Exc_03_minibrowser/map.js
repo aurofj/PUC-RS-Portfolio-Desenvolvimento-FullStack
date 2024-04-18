5b89941782971ca738d9d19aa9d90b7d2cf69e28
Auro <aurofj@gmail.com>
2024-04-18 11:14:24 -0300

    Meu primeiro Commit

---
+++ 01_Fundamentos_de_computação/01_Fundamentos de computação e Algoritmos/Exc_03_minibrowser/map.js
@@ -0,0 +1,14 @@
+// TTD Test Driven Development
+var novoMapa = new Map();
+
+class HistoryMap {
+    
+}
+
+
+// 11/11/2011 as 11 da noite
+novoMapa.set('11/11/2011 - 23:30 - URL',{title: 'Titulo do site', url: 'sozinha'});
+novoMapa.set('11/11/2011 - 23:31 - URL',{});
+
+var resultado = novoMapa;
+console.log(resultado);
